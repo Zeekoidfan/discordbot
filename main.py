bbf20b3292216222bc6d3f8b1ffc350edd47ee60
Ben <benjamin.liu.0818@gmail.com>
2022-04-25 02:08:50 +1000

    update requirements

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import pickle
 import numpy as np
 import nltk
+
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
