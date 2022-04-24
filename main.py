731d31a7d4ce19cf4c8456726690c07b9722ddc3
Ben <benjamin.liu.0818@gmail.com>
2022-04-25 02:11:52 +1000

    update requirements

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import numpy as np
 import nltk
 
 from nltk.stem import WordNetLemmatizer
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer
 
