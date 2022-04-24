c4e9653a42994e7852318e025a040c45a56a8d09
Ben <benjamin.liu.0818@gmail.com>
2022-04-25 01:36:49 +1000

    delete replit files

--- main.py
+++ main.py
@@ -16,32 +16,32 @@ intents = json.loads(open('intents.json').read())
 words = []
 classes = []
 documents = []
-ignore = ['?','!','.',',']
+ignore = ['?', '!', '.', ',']
 
-for intent in intents['Genral']:
-  for pattern in intent['patterns']:
-    world_list = nltk.word_tokenize(pattern)
-    words.append(world_list)
-    documents.append((world_list,intent['tag']))
-    if intent['tag'] not in classes:
-      classes.append(intent['tag'])
+for intent in intents['General']:
+    for pattern in intent['patterns']:
+        world_list = nltk.word_tokenize(pattern)
+        words.append(world_list)
+        documents.append((world_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
 
-print (documents)
-    
+print(documents)
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-  print('awjnjkawfjaw'.format(client))
+    print('awjnjkawfjaw'.format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  if message.content.startswith('!BOB'):
-    await message.channel.send('Hellowworld')
+    if message.author == client.user:
+        return
+    if message.content.startswith('!BOB'):
+        await message.channel.send('Hellowworld')
+
 
 client.run(os.getenv('DiscordToken'))
-
-  
